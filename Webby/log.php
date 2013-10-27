e75a6e8f916f6777e8b35b3359155e9d2bded59d
Mr Dr Prof Westwood <joewestwood159@gmail.com>
2013-10-27 10:29:01 +0000

    Invited JSON to the party

--- Webby/log.php
+++ Webby/log.php
@@ -1,45 +1,35 @@
-<pre>
-<h2>Relive those charming conversations</h2>
 <?php
-
-$phone = isset( $_REQUEST['phone'] ) ? $_REQUEST['phone'] : '441234567890';
-?>
-<form action="." method="get" >
-  <label>Enter your phone number</label>
-  <input type="text" name="phone" value="<?php echo $phone; ?>" />
-</form>
-<?php
-
-
 include 'db.php';
-$connection = mysqli_connect( $db["host"], $db["user"], $db["password"], $db["database"] );
-if( mysqli_connect_errno($connection) ):
+
+// Get phone number
+$phone = isset( $_REQUEST['phone'] ) ? $_REQUEST['phone'] : '';
+if( !$phone )
+{
+  http_response_code(401);
+  echo 'No phone number specified!';
+  exit;
+}
+
+// connect to DB
+$con = mysqli_connect( $db["host"], $db["user"], $db["password"], $db["database"] );
+if( mysqli_connect_errno($con) ):
+  http_response_code(500);
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit;
 endif;
 
-$phone = mysqli_real_escape_string( $connection, $phone );
+// Escape phone number
+$phone = mysqli_real_escape_string( $con, $phone );
+
+// Query Database for conversation thread
 $query = "SELECT * FROM `log` WHERE `from` = '".$phone."' OR `to` = '".$phone."'";
-$result = mysqli_query( $connection, $query );
+$result = mysqli_query( $con, $query );
+mysqli_close($con);
 
-echo "Query: ".$query."\n";
-echo "<h2>Result:</h2>";
-echo "Rows: ".$result->num_rows."\n";
+// Prepeare response data
+$output = [];
+while( $row = mysqli_fetch_array($result) ):
+  $output[] = $row;
+endwhile;
 
-?>
-<div id="conversation">
-  <?php while( $row = mysqli_fetch_array($result) ):
-    $bot = $row['from'] == 'BOT';
-  ?>
-    <div class="message <?php if( $bot ){ echo 'bot'; } ?>">
-      <strong><?php echo $row['from']; ?></strong>
-      <p><?php echo $row['content']; ?></p>
-      <time><?php echo date( "h:ia", $row['time']/1000 ); ?></time>
-    </div>
-  <?php endwhile; ?>
-</div>
-<?php
-
-mysqli_close($connection);
-
-?>
-</pre>
+echo json_encode($output);
