c1728c608a86f40724fb3cfe1569995662625e5f
andyish <ishera02@mc-s083600.local>
2013-10-27 11:48:50 +0000

    Added a summery

--- Webby/index.php
+++ Webby/index.php
@@ -66,7 +66,7 @@
               <div class="control-group"> 
                 <div class="right">
                   <div class="row">
-                    <input type="text" id="inputPhone" placeholder="447527156515">  
+                    <input type="text" id="inputPhone" placeholder="447860033078">  
                     <button type="submit" class="btn">Sign in</button>
 
                   </div>
@@ -77,12 +77,31 @@
           </div>
 
           <div class="span6">
-            <h4>Message History for <span id="msgHist">...</span></h4>
+            <h3>Message History for <span id="msgHist">...</span></h4>
           </div>
         </div>
         <hr>
+        <p class="text-center">MobilePhone --> Clockwork --> WebPage --> Service --> CleverBot --> Service --> Clockwork --> MobilePhone</p>
+        <hr>
     <!-- ABOUT -->
+        <h3>What does it do?</h3>
 
+        <ol>
+          <li>Mobile phone user sends a message</li>  
+          <li>Clockwork receives a message and posts it to a hosted page</li>  
+          <li>Hosted pages handles and logs it and forwards it to a local service</li>  
+          <li>Service manages chat sessions and sends the message to CleverBot</li>  
+          <li>CleverBot replys to the message</li>  
+          <li>Service sends the message to the message sender via clockwork api</li>  
+          <li>Mobile Phone user receives a reply</li>  
+        </ol>
+
+<!--
+        <p>So it all starts with a text being sent to <strong>07860033078</strong> where Clockwork sends a post containing the relevant data to a url on our server.</p>
+        <p>Then our server gets the data that we need from the post, then opens a socket to a service thats also running on the server and sends it the message.</p>
+        <p>The server thats running maintains a list of current sessions (coverstations) that are currently active (ones that have had much activity are periodically dropped). If there isn't a live session it requests a new one and store it.</p>
+        <p>The message is then sent to CleverBot and waits for a reply. Once it gets a reply it sends it back to the sender using the Clockwork API.</p>
+-->
 
       </section>
     </section>
